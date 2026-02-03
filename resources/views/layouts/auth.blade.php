a25d947504a461e753809722fb69063f2ef07276
Ramzan <muhammadramzan3043@gmail.com>
2026-02-03 15:18:32 +0500

    ramzan

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name') }} - Auth</title>
+    <title>{{ config('app.name') }} | Login</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
