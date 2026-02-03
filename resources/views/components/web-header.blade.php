7242989b87ebb05aa35f8ca9dffc88a3400d4d37
Iqrash122 <xagent891@gmail.com>
2026-02-03 12:00:02 +0500

    'iqrash

--- resources/views/components/web-header.blade.php
+++ resources/views/components/web-header.blade.php
@@ -1,34 +1,34 @@
 <header class="w-full bg-white shadow-md">
-    <div class="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+    <div class="container mx-auto px-6 py-4 flex items-center justify-between">
 
-        <div class="text-2xl font-bold text-indigo-600">
-            {{ config('app.name', 'LMS') }}
+        <div class="text-center mb-8 flex justify-center">
+            <img src="{{ asset('/assets/logo/logo.png') }}" alt="LOGO" class="w-24 h-24 object-contain">
         </div>
 
         <nav class="hidden md:flex items-center gap-8 text-gray-700 font-medium">
-            <a href="#" class="hover:text-indigo-600 transition">Home</a>
-            <a href="#" class="hover:text-indigo-600 transition">Courses</a>
-            <a href="#" class="hover:text-indigo-600 transition">About</a>
-            <a href="#" class="hover:text-indigo-600 transition">Contact</a>
+            <a href="#" class="hover:text-(--primary) transition">Home</a>
+            <a href="#" class="hover:text-(--primary) transition">Courses</a>
+            <a href="#" class="hover:text-(--primary) transition">About</a>
+            <a href="#" class="hover:text-(--primary) transition">Contact</a>
         </nav>
 
         <div class="flex items-center gap-3">
 
-            <a href="{{ route('login') }}"
-                class="px-5 py-2 rounded-xl border-2 border-indigo-600 text-indigo-600 font-semibold
-                      hover:bg-indigo-600 hover:text-white transition-all duration-300">
+            <a href="#"
+                class="px-5 py-2 rounded-xl border-2 border-gray-800 text-gray-800 font-semibold
+                      hover:bg-(--primary)  hover:border-(--primary) hover:text-white transition-all duration-300 ">
                 Student Portal
             </a>
 
-            <a href="{{ route('login') }}"
-                class="px-5 py-2 rounded-xl border-2 border-purple-600 text-purple-600 font-semibold
-                      hover:bg-purple-600 hover:text-white transition-all duration-300">
+            <a href="#"
+                class="px-5 py-2 rounded-xl border-2 border-gray-800 text-gray-800 font-semibold
+                      hover:bg-(--primary)  hover:border-(--primary) hover:text-white transition-all duration-300 ">
                 Instructor Portal
             </a>
 
-            <a href="{{ route('login') }}"
+            <a href="#"
                 class="px-5 py-2 rounded-xl border-2 border-gray-800 text-gray-800 font-semibold
-                      hover:bg-gray-800 hover:text-white transition-all duration-300">
+                      hover:bg-(--primary)  hover:border-(--primary) hover:text-white transition-all duration-300">
                 Admin Portal
             </a>
 
